d046586b721586df0fa4496097f17a835300dfc8
Moon Knight <103847581+Twilight-Killer@users.noreply.github.com>
2023-11-17 12:54:34 +0700

    Update whois.py

--- MukeshRobot/modules/whois.py
+++ MukeshRobot/modules/whois.py
@@ -22,10 +22,10 @@ def ReplyCheck(message: Message):
 infotext = (
     "[{full_name}](tg://user?id={user_id})\n\n"
     " ➻ ᴜsᴇʀ ɪᴅ: `{user_id}`\n"
-    " ➻ ғɪʀsᴛ ɴᴀᴍᴇ: `{first_name}`\n"
-    " ➻ ʟᴀsᴛ ɴᴀᴍᴇ: `{last_name}`\n"
+    " ➻ ɴᴀᴍᴀ ᴅᴇᴘᴀɴ: `{first_name}`\n"
+    " ➻ ɴᴀᴍᴀ ʙᴇʟᴀᴋᴀɴɢ: `{last_name}`\n"
     " ➻ ᴜsᴇʀɴᴀᴍᴇ: `@{username}`\n"
-    " ➻ ʟᴀsᴛ sᴇᴇɴ: `{last_online}`"
+    " ➻ ᴛᴇʀᴀᴋʜɪʀ ᴛᴇʀʟɪʜᴀᴛ: `{last_online}`"
 )
 
 
