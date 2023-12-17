822140433accec7ff56b61727c204e8a2a6d100b
Moon Knight <103847581+Twilight-Killer@users.noreply.github.com>
2023-12-17 08:54:37 +0700

    Update global_bans.py

--- MukeshRobot/modules/global_bans.py
+++ MukeshRobot/modules/global_bans.py
@@ -486,7 +486,7 @@ def __stats__():
 
 def __user_info__(user_id):
     is_gbanned = sql.is_user_gbanned(user_id)
-    text = "Malicious: <b>{}</b>"
+    text = "Jahat: <b>{}</b>"
     if user_id in [777000, 1087968824]:
         return ""
     if user_id == dispatcher.bot.id:
