fef23531efb6bdd58e144036dd9819a4c57880ab
Moon Knight <103847581+Twilight-Killer@users.noreply.github.com>
2024-01-07 07:31:39 +0700

    Update paste.py

--- MukeshRobot/modules/paste.py
+++ MukeshRobot/modules/paste.py
@@ -34,7 +34,7 @@ async def isPreviewUp(preview: str) -> bool:
 @capture_err
 async def paste_func(_, message):
     if not message.reply_to_message:
-        return await message.reply_text("Reply To A Message With /paste")
+        return await message.reply_text("Balas Pesan Dengan /paste")
     m = await message.reply_text("Pasting...")
     if message.reply_to_message.text:
         content = str(message.reply_to_message.text)
@@ -43,22 +43,22 @@ async def paste_func(_, message):
         if document.file_size > 1048576:
             return await m.edit("You can only paste files smaller than 1MB.")
         if not pattern.search(document.mime_type):
-            return await m.edit("Only text files can be pasted.")
+            return await m.edit("Hanya file teks yang dapat di paste.")
         doc = await message.reply_to_message.download()
         async with aiofiles.open(doc, mode="r") as f:
             content = await f.read()
         os.remove(doc)
     link = await paste(content)
-    preview = link + "/preview.png"
+    preview = link 
     button = InlineKeyboard(row_width=1)
     button.add(InlineKeyboardButton(text="• ᴘᴀsᴛᴇ ʟɪɴᴋ •", url=link))
 
-    if await isPreviewUp(preview):
-        try:
-            await message.reply_photo(photo=preview, quote=False, reply_markup=button)
-            return await m.delete()
-        except Exception:
-            pass
+    
+    try:
+        await message.reply("ᴘᴀsᴛᴇ ʟɪɴᴋ", quote=False, reply_markup=button)
+            #return await m.delete()
+    except Exception:
+        pass
     return await m.edit(link)
 
 
