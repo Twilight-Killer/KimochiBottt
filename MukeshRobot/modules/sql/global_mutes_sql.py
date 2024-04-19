3a9e8b59c94d714f60bd75a4128ef58b239f3678
Moon Knight <103847581+Twilight-Killer@users.noreply.github.com>
2024-04-19 23:47:58 +0700

    Update global_mutes_sql.py

--- MukeshRobot/modules/sql/global_mutes_sql.py
+++ MukeshRobot/modules/sql/global_mutes_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, UnicodeText, BigInteger, String, Boolean
 
-from VegetaRobot.modules.sql import BASE, SESSION
+from MukeshRobot.modules.sql import BASE, SESSION
 
 
 class GloballyMutedUsers(BASE):
